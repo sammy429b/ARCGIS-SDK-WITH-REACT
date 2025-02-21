632d23073462365ae4f3c4ed42d86760ee8c3a27
sammy429b <samarthnbahirgonde49@gmail.com>
2025-02-21 17:12:51 +0530

    Created ArcMapView component

---
+++ src/components/Arc/ArcMapView/ArcMapView.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useRef } from "react";
+import MapView from "@arcgis/core/views/MapView";
+import Map from "@arcgis/core/Map";
+
+
+export default function ArcMapView() {
+
+    const mapRef = useRef(null);
+
+    useEffect(() => {
+        if (!mapRef.current) return; // Ensure ref exists before using it
+
+        console.log("Initializing ArcGIS Map...");
+
+        const map = new Map({
+            basemap: "streets",
+        });
+
+        const mapView = new MapView({
+            container: mapRef.current,
+            map: map,
+            center: [77.4126, 23.2599], // Longitude, Latitude (Madhya Pradesh)
+            zoom: 5, // Adjust zoom level as needed
+        });
+
+        return () => {
+            if (mapView) {
+                mapView.container = null; // Properly remove reference
+                mapView.destroy(); // Cleanup ArcGIS resources
+            }
+        };
+    }, []);
+    return (
+        <>
+            <div ref={mapRef} className="w-full h-[100vh]"></div>
+        </>
+    )
+}
